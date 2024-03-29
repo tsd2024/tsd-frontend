fd24c4b3d63969cec4fec4a1a30ccb85a030278b
Jakub Mrówczyński <jakub@mrowczynski.net>
2024-03-29 19:21:35 +0100

    updated room lobby layout and added topbar

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -33,75 +33,68 @@ export default function RoomPage({ params }: { params: { roomId: string } }) {
     };
 
     return (
-        <div className="flex flex-col w-full h-screen">
-            
-            <div className="flex flex-row w-full h-screen">
-                <div className="flex flex-col border-r w-1/5 p-4">
-                    <h1 className="text-2xl font-bold pb-4">
-                        Players
-                    </h1>
-
-                    {/* Players' cards */}
-                    <div className="flex flex-col space-y-4">
-                        {joinedPlayers.map((player) => (
-                            <Card key={player}>
-                                <CardHeader>
-                                    <CardTitle>{player}</CardTitle>
-                                    <CardDescription>
-                                        <div className="flex flex-row space-x-4 items-center">
-                                            <p>choosing card</p>
-                                            <div className="w-3 h-3 rounded-full bg-yellow-300"></div>
-                                        </div>
-                                    </CardDescription>
-                                </CardHeader>
-                            </Card>
-                        ))}
-                    </div>
-
-
+        <div className="flex flex-row w-full h-screen">
+            <div className="flex flex-col border-r w-1/5 p-4">
+                <h1 className="text-2xl font-bold pb-4">
+                    Players
+                </h1>
+                {/* Players' cards */}
+                <div className="flex flex-col space-y-4">
+                    {joinedPlayers.map((player) => (
+                        <Card key={player}>
+                            <CardHeader>
+                                <CardTitle>{player}</CardTitle>
+                                <CardDescription>
+                                    <div className="flex flex-row space-x-4 items-center">
+                                        <p>choosing card</p>
+                                        <div className="w-3 h-3 rounded-full bg-yellow-300"></div>
+                                    </div>
+                                </CardDescription>
+                            </CardHeader>
+                        </Card>
+                    ))}
                 </div>
-                <div className='flex flex-col w-4/5 p-4'>
-                    <div className="flex flex-row w-full justify-between">
-                        <h1 className="text-3xl font-bold">Room {roomId}</h1>
-                        <div className="flex flex-row space-x-4 items-center">
-                            <Button variant={"secondary"}>Invite players</Button>
-                            <GoTasklist className="ml-4" style={{ height: '100%', width: 'auto' }}/>
-                        </div>
-
+            </div>
+            <div className='flex flex-col w-4/5 p-4'>
+                {/* Header */}
+                <div className="flex flex-row w-full justify-between">
+                    <h1 className="text-3xl font-bold">Room {roomId}</h1>
+                    <div className="flex flex-row space-x-4 items-center mr-2">
+                        <Button variant={"outline"}>Invite players</Button>
+                        <Button variant={"outline"}>
+                            <GoTasklist className="h-full w-auto"/>
+                        </Button>
                     </div>
-
-
-                    {/* Planning Poker Section */}
-                    <div className="relative w-full h-3/4 flex justify-center items-center">
-                        {/* Planning Poker Rectangle */}
-                        <div className="absolute inset-0 flex justify-center items-center">
-                            {/* Content of the Rectangle */}
-                            <div className="w-2/5 h-1/5 bg-blue-500 rounded-lg flex justify-center items-center">
-                                <h1 className="text-white text-xl font-bold">Pick your cards!</h1>
-                            </div>
+                </div>
+                {/* Planning Poker Section */}
+                <div className="relative w-full h-3/4 flex justify-center items-center flex-col space-y-6">
+                    <div className="w-2/5 rounded-3xl p-px bg-gradient-to-b from-blue-300 to-pink-300 dark:from-blue-800 dark:to-purple-800 ">
+                        <div className="rounded-[calc(1.5rem-1px)] p-10 bg-white dark:bg-gray-900 flex items-center justify-center">
+                            <p className="text-gray-700 dark:text-gray-300">Pick your cards!</p>
                         </div>
                     </div>
-
-
-                    <div className="space-y-4">
-                        <p className="flex items-center justify-center">
-                            Choose your card
-                        </p>
-
-                        {/* Cards */}
-                        <div className="flex flex-row space-x-4 items-center justify-center">
-                            {cards.map((card) => (
-                                <Button
-                                    variant={"secondary"}
-                                    key={card}
-                                    className={`w-12 h-20 rounded-lg shadow-md transition-transform duration-300 transform hover:-translate-y-1.5 ${selectedCard === card ? 'bg-green-400 hover:bg-green-400' : ''
-                                        }`}
-                                    onClick={() => handleCardSelection(card)}
-                                >
-                                    {card}
-                                </Button>
-                            ))}
-                        </div>
+                    {/* This button should be only available for lobby */}
+                    <Button variant="default">
+                        Show cards
+                    </Button>
+                </div>
+                {/* Cards */}
+                <div className="space-y-4">
+                    <p className="flex items-center justify-center">
+                        Choose your card
+                    </p>
+                    <div className="flex flex-row space-x-4 items-center justify-center">
+                        {cards.map((card) => (
+                            <Button
+                                variant={"outline"}
+                                key={card}
+                                className={`w-12 h-20 rounded-lg shadow-md transition-transform duration-300 transform hover:-translate-y-1.5 ${selectedCard === card ? 'bg-green-400 hover:bg-green-400' : ''
+                                    }`}
+                                onClick={() => handleCardSelection(card)}
+                            >
+                                {card}
+                            </Button>
+                        ))}
                     </div>
                 </div>
             </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard", "/create-room"] }
+export const config = { matcher: ["/dashboard", "/create-room", "/rooms/:path*"] }
