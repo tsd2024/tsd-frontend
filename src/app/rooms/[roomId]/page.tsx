2c7f0de8b4f10e883bce8120b4eeded962043f52
Jakub Mrówczyński <jakub@mrowczynski.net>
2024-05-05 12:00:03 +0200

    feat: game room code cleanup, seperated into componenets

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -3,18 +3,13 @@
 import React from 'react'
 import { Button } from '@/components/ui/button';
 
-import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-
 import { GoTasklist } from "react-icons/go";
 import { useSearchParams } from 'next/navigation'
 
 import { LobbyResultSheet } from './LobbyResultSheet';
 import useGameLogic from './use-game-logic';
+import { GameTable } from './GameTable';
+import { GameRoomSidePanel } from './GameRoomSidePanel';
 
 export default function RoomPage({ params }: { params: { roomId: string } }) {
     const searchParams = useSearchParams()
@@ -36,27 +31,9 @@ export default function RoomPage({ params }: { params: { roomId: string } }) {
 
     return (
         <div className="flex flex-row w-full h-screen">
-            <div className="flex flex-col border-r w-1/5 p-4">
-                <h1 className="text-2xl font-bold pb-4">
-                    Players
-                </h1>
-                {/* Players' cards */}
-                <div className="flex flex-col space-y-4">
-                    {joinedPlayers.map((player) => (
-                        <Card key={player.player_id}>
-                            <CardHeader>
-                                <CardTitle>{player.player_id}</CardTitle>
-                                <CardDescription>
-                                    <div className="flex flex-row space-x-4 items-center">
-                                        <p>{player.ready ? 'Ready' : 'Choosing card'}</p>
-                                        <div className={`w-3 h-3 rounded-full ${player.ready ? 'bg-green-400' : 'bg-yellow-300'}`}></div>
-                                    </div>
-                                </CardDescription>
-                            </CardHeader>
-                        </Card>
-                    ))}
-                </div>
-            </div>
+            <GameRoomSidePanel 
+                joinedPlayers={joinedPlayers} 
+            />
             <div className='flex flex-col w-4/5 p-4'>
                 {/* Header */}
                 <div className="flex flex-row w-full justify-between">
@@ -68,14 +45,11 @@ export default function RoomPage({ params }: { params: { roomId: string } }) {
                         </Button>
                     </div>
                 </div>
+
                 {/* Planning Poker Section */}
                 <div className="relative w-full h-3/4 flex justify-center items-center flex-col space-y-6">
-                    <div className="w-2/5 rounded-3xl p-px bg-gradient-to-b from-blue-300 to-pink-300 dark:from-blue-800 dark:to-purple-800 ">
-                        <div className="rounded-[calc(1.5rem-1px)] p-10 bg-white dark:bg-gray-900 flex items-center justify-center">
-                            <p className="text-gray-700 dark:text-gray-300">Pick your cards!</p>
-                        </div>
-                    </div>
-                    {/* This button should be only available for lobby admin*/}
+                    <GameTable />
+                    {/* Show cards and results buttons*/}
                     <div className="flex flex-row space-x-2">
                         {isAdmin && (
                             <Button variant="default" onClick={revealCards} className={roundConcluded ? "w-1/2" : "w-full"} >
@@ -95,8 +69,8 @@ export default function RoomPage({ params }: { params: { roomId: string } }) {
                             roundConcluded={roundConcluded}
                         />
                     </div>
-
                 </div>
+
                 {/* Cards */}
                 <div className="space-y-4">
                     <p className="flex items-center justify-center">
