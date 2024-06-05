fbce7966a7ab2baa4ca437ba949c94f5a9c5a2e5
Jakub Mrówczyński <jakub@mrowczynski.net>
2024-06-05 20:53:35 +0200

    feat: test new middleware for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,18 @@
-export { default } from "next-auth/middleware"
+import { NextRequest, NextResponse } from 'next/server';
+
+export default async function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+    const session = !!req.cookies.get("next-auth.session-token")
+
+    if (!session) {
+        return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
+    }
+    return NextResponse.next();
+}
 
 export const config = { matcher: ["/dashboard", "/create-room", "/rooms/:path*", "/join"] }
+
+// export { default } from "next-auth/middleware"
+
+// export const config = { matcher: ["/dashboard", "/create-room", "/rooms/:path*", "/join"] }
 
